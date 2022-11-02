eb468a452a3cd51677a685aa892bc0f68ffa5026
Peter Wilmshurst <wilmshurstp@rsc.org>
2022-11-02 08:22:35 +0000

    add metadata to template

--- pages/restaurants/index.vue
+++ pages/restaurants/index.vue
@@ -1,5 +1,14 @@
 <template>
 <div>
+     <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+  <Head>
+    <Title>Top 50 Restaurants in the World</Title>
+  <Meta
+  name="description"
+  content="My page is cool" />
+  <Link rel="preload" href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png" as="script" />
+  </Head>
+  </Html>
  <NuxtLayout name="custom">
 <div class="container">
     <RestaurantTable />
